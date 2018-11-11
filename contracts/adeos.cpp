1c431ddec825c0587c8a1ffd737d1413b86729aa
Ian Macalinao <me@ian.pw>
2018-11-10 23:17:41 -0800

    make abi compile

--- contracts/adeos.cpp
+++ contracts/adeos.cpp
@@ -16,54 +16,54 @@ class [[eosio::contract]] adeos : public eosio::contract {
       });
     }
 
-    [[eosio::action]]
-    void paydividends() {
-      // get total EOS balance of contract
-      accounts accountstable("eosio.token"_n, _code.value);
-      symbol coresymbol(symbol_code("EOS"), 4);
-      const auto& ac = accountstable.get(coresymbol.code());
+    // [[eosio::action]]
+    // void paydividends() {
+    //   // get total EOS balance of contract
+    //   accounts accountstable("eosio.token"_n, _code.value);
+    //   symbol coresymbol(symbol_code("EOS"), 4);
+    //   const auto& ac = accountstable.get(coresymbol.code());
 
-      // compute payout per person
-      payout_index payouts(_self, _self);
+    //   // compute payout per person
+    //   payout_index payouts(_self, _self);
 
-      for (auto itr = payouts.begin(); itr != payouts.end(); itr++) {
+    //   for (auto itr = payouts.begin(); itr != payouts.end(); itr++) {
 
-        auto size = std::count(payout_index.cbegin(), payout_index.cend());
-        auto amtPerPerson = ac.balance / size;
+    //     auto size = std::count(payout_index.cbegin(), payout_index.cend());
+    //     auto amtPerPerson = ac.balance / size;
    
 
-         action(
-        permission_level{ _code, N(active) },
-        N(eosio.token), N(transfer),
-        std::make_tuple(_code, itr->payee, amtPerPerson, std::string(""))
-     ).send();
-      }
-    }
+    //      action(
+    //     permission_level{ _code, N(active) },
+    //     N(eosio.token), N(transfer),
+    //     std::make_tuple(_code, itr->payee, amtPerPerson, std::string(""))
+    //  ).send();
+    //   }
+    // }
 
-    [[eosio::action]]
-    void depositfunds() {
-      auto transfer_data = unpack_action_data<st_transfer>();
-      const auto from = transfer_data.from;
+    // [[eosio::action]]
+    // void depositfunds() {
+    //   auto transfer_data = unpack_action_data<st_transfer>();
+    //   const auto from = transfer_data.from;
 
-      // add payee
-      payout_index payouts(_self, _self);
-      payouts.emplace(_self, [&](auto& payout) {
-        payout.payee = N(from);
-      });
-    }
+    //   // add payee
+    //   payout_index payouts(_self, _self);
+    //   payouts.emplace(_self, [&](auto& payout) {
+    //     payout.payee = N(from);
+    //   });
+    // }
 
 
   private: 
-      struct payout {
-        name payee;
-      }
-    typedef eosio::multi_index< N(payout), payout > payout_index;
+    //   struct payout {
+    //     name payee;
+    //   }
+    // typedef eosio::multi_index< N(payout), payout > payout_index;
 
-    struct account {
-      asset    balance;
-      uint64_t primary_key()const { return balance.symbol.code(); }
-    };
-    typedef eosio::multi_index<"accounts"_n, account> accounts;
+    // struct account {
+    //   asset    balance;
+    //   uint64_t primary_key()const { return balance.symbol.code(); }
+    // };
+    // typedef eosio::multi_index<"accounts"_n, account> accounts;
 
     struct [[eosio::table]] space {
       name key;
@@ -75,4 +75,5 @@ class [[eosio::contract]] adeos : public eosio::contract {
   
 };
 
-EOSIO_DISPATCH( adeos, (setimage)(paydividends)(depositfunds))
+EOSIO_DISPATCH( adeos, (setimage))
+// EOSIO_DISPATCH( adeos, (setimage)(paydividends)(depositfunds))
